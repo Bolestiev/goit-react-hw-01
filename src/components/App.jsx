959c91ac7203980252ff9682047face3cd2372fd
Anton Bolestiev <antonbolestiev1997@gmail.com>
2024-10-28 14:38:39 +0200

    add changes task 2 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,25 @@ import "modern-normalize";
 import "./App.css";
 import Profile from "./Profile/Profile";
 import userData from "../assets/user.Data.json";
+import friends from "../assets/friends.json";
+import FriendList from "./FriendList/FriendList";
+import transactions from "../assets/transaction.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 const App = () => {
   console.log(userData);
   return (
-    <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-    />
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
 
