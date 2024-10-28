b5f45c02d7e0578c0046b367d433d5faa2ba2f80
Anton Bolestiev <antonbolestiev1997@gmail.com>
2024-10-28 11:57:46 +0200

    add changes task1

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import "../index.css";
+import "modern-normalize";
+import "./App.css";
+import Profile from "./Profile/Profile";
+import userData from "./../assets/user.Data.json";
+
+const App = () => {
+  console.log(profileData);
+  return (
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
+  );
+};
+
+export default App;
